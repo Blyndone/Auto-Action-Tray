cd546e2d8b22fad42c6ec304a627861aca9f0d47
Rick Duel <81060949+Blyndone@users.noreply.github.com>
2025-03-18 00:42:22 -0400

    Refactor: Tooltip - Add support for Multiple damge Values

--- apps/helpers/damageCalc.js
+++ apps/helpers/damageCalc.js
@@ -4,140 +4,191 @@ export class DamageCalc {
     options.data.root['actionType'] = '';
     options.data.root['saveType'] = '';
     let currentTray = options.data.root.currentTray;
-    let min = 0,
-      dieSize = 0,
-      max = 0,
-      bonus = 0,
-      saveType = '',
-      saveDc = 0,
-      damageType = '';
+    let bonus = 0,
+      saveType = '';
 
-    let actionType = item.system?.activities?.contents[0]?.type;
+    let activities = this.getActivities(item);
+    let damageParts = activities.map((activity) => this.getDamage(activity));
+    let flatDamages = activities.map((activity) =>
+      this.getFlatDamage(activity, item.actor)
+    );
+    let saveDc = activities.map((activity) => this.getSaveDc(activity));
 
-    switch (true) {
-      case item.type == 'weapon': {
-        const baseDamage = item.system.damage.base;
-        if (baseDamage.number && baseDamage.denomination) {
-          min = baseDamage.number; // Minimum damage (1 * number of dice)
-          max = baseDamage.number * baseDamage.denomination; // Maximum damage
-          dieSize = baseDamage.denomination; // Die size
+    let scaling = this.getScaling(item, currentTray.spellLevel);
 
-          let ability = item.system.activities.contents[0].attack.ability;
-          if (ability == '') {
-            ability =
-              item.system.activities.contents[0].attack.type.value == 'melee'
-                ? 'str'
-                : 'dex';
-          }
+    let i = 0;
+    let activity = activities[i];
 
-          bonus = item.parent.system.abilities[ability].mod; // Bonus damage
-
-          damageType =
-            baseDamage.types.size > 0
-              ? this.capitalize(baseDamage.types.first())
-              : 'Damage';
-        }
-        break;
-      }
-      case item.type == 'spell' && actionType == 'heal': {
-        let scaling = {};
-        if (item.system.activities.contents[0].canScaleDamage) {
-          scaling = this.getScaling(item, currentTray.spellLevel);
-        }
-
-        const baseDamage = item.system.activities.contents[0].healing;
-        if (baseDamage?.number && baseDamage?.denomination) {
-          min = baseDamage.number; // Minimum healing (1 * number of dice)
-          max = baseDamage.number * baseDamage.denomination; // Maximum healing
-          dieSize = baseDamage.denomination; // Die size
-          bonus = baseDamage.bonus; // Bonus healing
-          let numberDice;
-          if (scaling['scaling']) {
-            numberDice = scaling['scaling'] * scaling['number'];
-
-            min += numberDice;
-            max += numberDice * baseDamage.denomination;
-          }
-          if (bonus == '@mod') {
-            bonus =
-              item.parent.system.abilities[
-                item.parent.system.attributes.spellcasting
-              ].mod;
-          }
-          damageType =
-            baseDamage.types.size > 0
-              ? this.capitalize(baseDamage.types.first())
-              : 'Healing';
-          damageType = 'Healing';
-        }
-        break;
-      }
-      case item.type == 'spell' &&
-        (actionType == 'attack' ||
-          actionType == 'save' ||
-          actionType == 'damage'): {
-        let scaling = {};
-        if (item.system.activities.contents[0].canScaleDamage) {
-          scaling = this.getScaling(item, currentTray.spellLevel);
-        }
-        const baseDamage = item.system.activities.contents[0].damage.parts[0];
-        if (baseDamage?.number && baseDamage?.denomination) {
-          min = baseDamage.number; // Minimum healing (1 * number of dice)
-          max = baseDamage.number * baseDamage.denomination; // Maximum healing
-          dieSize = baseDamage.denomination; // Die size
-          bonus = baseDamage.bonus; // Bonus healing
-          let numberDice;
-          if (scaling['scaling']) {
-            numberDice = scaling['scaling'] * scaling['number'];
-
-            min += numberDice;
-            max += numberDice * baseDamage.denomination;
-          }
-          if (actionType == 'save') {
-            saveType = item.system.activities.contents[0].save.ability.first();
-            saveDc = item.system.activities.contents[0].save.dc.value;
-          }
-
-          damageType =
-            baseDamage.types.size > 0
-              ? this.capitalize(baseDamage.types.first())
-              : 'Damage';
-        }
-
-        break;
-      }
-
-      case item.type == 'consumable': {
-        break;
-      }
-      default:
-        // console.log('default-' + item.name);
-        break;
-    }
-
-    bonus = bonus != '' ? parseInt(bonus) : 0;
 
     options.data.root['actionType'] = this.capitalize(
-      item.system?.activities?.contents[0]?.activation?.type
+      item.system?.activities?.contents[i]?.activation?.type
     );
+    let dice = ['<i class="fa-solid fa-dice-d6"></i>'];
+    let damage = [];
+    let totalDamage = { min: 0, max: 0 };
 
-    if (min == 0) return;
-    if (min === max) return `${min} ${damageType}`;
-
-    if (saveType != '') {
-      options.data.root[
-        'saveType'
-      ] = `    ${saveType.toUpperCase()} - DC ${saveDc}`;
+    if (scaling['scaling'] == undefined || isNaN(scaling['scaling'])) {
+      scaling['scaling'] = 0;
+      scaling['number'] = 0;
     }
-    options.data.root['diceFormula'] = `🎲${min}d${dieSize}${
-      bonus > 0 ? ' + ' + bonus : ''
-    } ${damageType}`;
 
-    min += bonus;
-    max += bonus;
-    return `${min} ~ ${max}  Damage`;
+    damageParts[i].forEach((part) => {
+      if (    flatDamages[i].length != 0) {
+        bonus = flatDamages[i][0] != '' ? parseInt(flatDamages[i]) : 0;
+      }
+      bonus = parseInt(bonus) + parseInt((part.bonus != '@mod' && part.bonus != '') ? part.bonus : 0);
+      dice.push(
+        `${part.min}d${part.dieSize}${bonus > 0 ? ' + ' + bonus : ''} ${[
+          this.capitalize(part.damageType),
+        ].join(' ')}`
+      );
+      totalDamage['min'] =
+        totalDamage['min'] +
+        part.min +
+        scaling['scaling'] * scaling['number'] +
+        bonus;
+      totalDamage['max'] =
+        totalDamage['max'] +
+        part.max +
+        scaling['scaling'] * scaling['number'] * part.dieSize +
+        bonus;
+    });
+
+    if (scaling['scaling'] != 0) {
+      dice.push(
+        `${scaling['scaling'] * scaling['number']}d${
+          damageParts[i][0]?.dieSize
+        }`
+      );
+    }
+
+    if (totalDamage['min'] != 0 && totalDamage['max'] != 0) {
+      damage.push(`${totalDamage['min']} ~ ${totalDamage['max']} `);
+      damage.push(
+        `${damageParts[i][0]?.damageType != 'healing' ? 'Damage' : 'Healing'} `
+      );
+    }
+
+    if (saveDc[i].saveType != '') {
+      options.data.root['saveType'] = `    ${saveDc[
+        i
+      ].saveType.toUpperCase()} - DC ${saveDc[i].saveDc}`;
+    }
+
+    if (damageParts[i][0]?.formula != '') {
+      damage = [damageParts[i][0]?.formula];
+      dice = [damageParts[i][0]?.formula];
+    }
+
+    options.data.root['diceFormula'] =
+      dice.length > 1 ? dice.slice(0, 1) + ' ' + dice.slice(1).join(' <br><i class="fa-solid fa-dice-d6"></i> ') : '';
+    return damage.join(' ');
   }
 
+  //returns [activies]
+  static getActivities(item) {
+    return item.system.activities.contents;
+  }
+  //returns [{min, dieSize, max, mod, damageType}]
+  static getDamage(activity) {
+    let damage = {
+      min: 0,
+      max: 0,
+      dieSize: 0,
+      bonus: 0,
+      damageType: '',
+    };
+    let damageParts = [];
+    switch (true) {
+      case activity.type == 'attack' ||
+        activity.type == 'damage' ||
+        activity.type == 'save' ||
+        activity.type == 'spell':
+        activity.damage.parts.forEach((part) => {
+          damageParts.push({
+            min: part.number,
+            max: part.number * part.denomination,
+            dieSize: part.denomination,
+            bonus: part.bonus,
+            damageType: [...part.types],
+            formula: part.custom.enabled ? part.custom.formula : '',
+          });
+        });
+        break;
+      case activity.type == 'heal':
+        let healing = activity.healing;
+        damageParts.push({
+          min: healing.number,
+          max: healing.number * healing.denomination,
+          dieSize: healing.denomination,
+          bonus: healing.bonus,
+          damageType: 'healing',
+          formula: healing.custom.enabled ? healing.custom.formula : '',
+        });
+        break;
+      case 'damage':
+
+      default:
+        return [];
+    }
+    return damageParts;
+  }
+
+  //return flat [Mod]
+  static getFlatDamage(activity, actor) {
+    let flatDamages = [];
+    let ability = '';
+    let modDamge = 0;
+    switch (true) {
+      case activity?.attack?.type.classification == 'weapon':
+        ability = activity.attack.ability;
+        if (ability == '') {
+          ability = activity.attack.type.value == 'melee' ? 'str' : 'dex';
+        }
+        modDamge = actor.system.abilities[ability].mod;
+        activity.damage.parts.forEach((part) => {
+          flatDamages.push(part.bonus + modDamge);
+        });
+        break;
+
+      case activity.type == 'spell':
+        ability = activity?.attack.ability;
+        ability =
+          activity.parent?.ability != ''
+            ? activity.parent.ability
+            : actor.system.attributes.spellcasting;
+
+        modDamge = actor.system.abilities[ability].mod;
+        activity.damage.parts.forEach((part) => {
+          flatDamages.push(part.bonus == '@mod' ? modDamge : part.bonus);
+        });
+        break;
+
+      case activity.type == 'heal':
+        ability =
+          activity.parent?.ability != '' && activity.parent.ability
+            ? activity.parent.ability
+            : actor.system.attributes.spellcasting;
+        modDamge = actor.system.abilities[ability]?.mod || 0;
+        flatDamages.push(
+          activity.healing.bonus == '@mod' ? modDamge : activity.healing.bonus
+        );
+        break;
+      default:
+        return [];
+    }
+    return flatDamages;
+  }
+  static getSaveDc(activity) {
+    if (activity.type == 'save') {
+      let saveType = activity.save.ability.first();
+      let saveDc = activity.save.dc.value;
+      return { saveType: saveType, saveDc: saveDc };
+    }
+    return { saveType: '', saveDc: '' };
+  }
+
+  //return { scaling: scaling, number: number, formula: formula };
   static getScaling(item, castLevel) {
     if (item.actorSpellData) {
       castLevel = item.actorSpellData.level;
@@ -175,20 +226,20 @@ export class DamageCalc {
 
   static capitalize(val) {
     return String(val).charAt(0).toUpperCase() + String(val).slice(1);
-    }
+  }
 
-    static CheckMultiHit() { 
-        let items = [
-    "Eldritch Blast",
-    "Magic Missile",
-    "Scorching Ray",
-    "Mass Healing Word",
-    "Mass Cure Wounds",
-    "Steel Wind Strike",
-    "Chain Lightning",
-    "Chromatic Orb",
-    "Chaos Bolt",
-    "Ice Knife"
-]
-    }
+  static CheckMultiHit() {
+    let items = [
+      'Eldritch Blast',
+      'Magic Missile',
+      'Scorching Ray',
+      'Mass Healing Word',
+      'Mass Cure Wounds',
+      'Steel Wind Strike',
+      'Chain Lightning',
+      'Chromatic Orb',
+      'Chaos Bolt',
+      'Ice Knife',
+    ];
+  }
 }
