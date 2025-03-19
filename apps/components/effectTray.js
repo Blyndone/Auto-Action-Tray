2ba7bc41bd8308ddcfa248e6c7e7b9c4bcfee687
Rick Duel <81060949+Blyndone@users.noreply.github.com>
2025-03-19 17:15:35 -0400

    Bug: Embeded Effect  Tooltip Items

--- apps/components/effectTray.js
+++ apps/components/effectTray.js
@@ -1,59 +1,69 @@
 export class EffectTray {
   constructor(options = {}) {
-    this.actor = options.actor;
-    this.effects = options.effects || [];
+    this.actor = options.actor
+    this.effects = options.effects || []
   }
 
   async setActor(actor) {
-    this.actor = actor;
-    await this.setEffects();
+    this.actor = actor
+    await this.setEffects()
   }
-  setEffects() {
-    let tmpArr = [];
+  async setEffects() {
+    let effectsPromises = this.actor.appliedEffects.map(async (effect) => {
+      let desc = await this.getDescription(effect)
+      return {
+        id: effect._id,
+        name: effect.name,
+        description: desc,
+        img: effect.img,
+        duration: effect.duration,
+        type: effect.type,
+        isTemporary: effect.isTemporary,
+      }
+    })
 
-    this.actor.appliedEffects.forEach(effect => {
-      this.getDescription(effect).then(desc => {
-        tmpArr.push({
-          id: effect._id,
-          name: effect.name,
-          description: desc,
-          img: effect.img,
-          duration: effect.duration,
-          type: effect.type,
-          isTemporary: effect.isTemporary
-        });
-      });
-    });
-    this.effects = tmpArr;
-  }
-
-  async getDescription(effect) {
-    let desc = effect.description;
-    let regex = /\[(\S+)/;
-    let match = desc.match(regex);
-    if (!match) return desc;
-    return await fromUuid(match[1]).then(item => {
-      return item?.text?.content || desc;
-    });
+    this.effects = await Promise.all(effectsPromises)
   }
 
   static async removeEffect(event, element) {
-    await foundry.applications.api.DialogV2
-      .confirm({
-        window: { title: `Delete Active Effect: ${event.dataset.name} ` },
-        content: `<p>Remove the Active Effect -  ${event.dataset.name}</p>`,
-        modal: true
-      })
-      .then(result => {
-        if (result) {
-          let id = event.dataset.effectId;
-          this.actor.effects.find(e => e._id == id).delete();
-          // this.actor.deleteEmbeddedEntity("ActiveEffect", id);
+    await foundry.applications.api.DialogV2.confirm({
+      window: { title: `Delete Active Effect: ${event.dataset.name} ` },
+      content: `<p>Remove the Active Effect -  ${event.dataset.name}</p>`,
+      modal: true,
+    }).then((result) => {
+      if (result) {
+        let id = event.dataset.effectId
+        this.actor.effects.find((e) => e._id == id).delete()
+      }
+    })
+  }
+
+  async getDescription(effect) {
+    let desc = effect.description
+    let regexAll = /@Embed\[(.*?)]/g
+    let matches = desc.match(regexAll)
+    if (!matches) return desc
+
+    let replacements = await Promise.all(
+      matches.map(async (match) => {
+        let innerMatch = match.match(/(?:@Embed\[)(.*?)(?:(\s.*?))(?:])/)
+        if (!innerMatch || !innerMatch[1]) return match
+        let replacement = await fromUuid(innerMatch[1])
+        return {
+          match,
+          replacement: replacement?.system?.description?.value || replacement.text.content || match,
         }
-      });
+      }),
+    )
+
+    replacements.forEach(({ match, replacement }) => {
+      desc = desc.replace(match, replacement)
+    })
+
+    return '<div>' + desc + '</div>'
   }
 
   parseUuid(string) {
-    return "uuid";
+    return 'uuid'
   }
 }
